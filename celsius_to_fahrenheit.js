8b25a7711b3186938b95500f57deb084c79741bd
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-19 18:23:10 -0500

    Add JavaScript version of the program

---
+++ celsius_to_fahrenheit.js
@@ -0,0 +1,14 @@
+// requirement: make sure to `npm install readline-sync` before running the script
+
+const FREEZING_ZERO = 32;
+
+function celsiusToFahrenheit(tempCelsius) {
+  return ((tempCelsius * 9 / 5) + FREEZING_ZERO);
+}
+
+let readlineSync = require('readline-sync');
+let tempCelsius = readlineSync.question("Enter the temperature in Celsius: ");
+console.log(`You entered: ${tempCelsius} C`);
+
+let convertedTemp = celsiusToFahrenheit(tempCelsius);
+console.log(`${tempCelsius} degrees Celsius is ${convertedTemp} degrees Fahrenheit.`);
